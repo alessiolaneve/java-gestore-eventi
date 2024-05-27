a172e0b792ff735220da8c060b0d52a3f209208f
Alessio La Neve <90204732+alessiolaneve@users.noreply.github.com>
2024-05-27 19:28:20 +0200

    step 2 in progress (non funziona)

--- src/org/java/gestore/eventi/Evento.java
+++ src/org/java/gestore/eventi/Evento.java
@@ -1,21 +1,7 @@
 package org.java.gestore.eventi;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-
-/*Step 1	
-
-
-Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
--prenota: aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha posti disponibili deve restituire 
-un messaggio di avviso.
-
--disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono prenotazioni restituisce un messaggio di avviso.
-l’override del metodo toString() in modo che venga restituita una stringa contenente: data formattata - titolo
-Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funzioni richieste.
-*/
-
-
+import java.time.format.DateTimeFormatter;
 
 /* Creare una classe Evento che abbia le seguenti proprietà:
 	-titolo
@@ -27,7 +13,7 @@ Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funz
 public class Evento {
 	
 	private String titolo;
-	private Date data;
+	private LocalDateTime data;
 	private int numPostiTot;
 	private int numPostiPren;
 	
@@ -35,19 +21,52 @@ public class Evento {
 /*  Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel costruttore,
 	tranne posti prenotati che va inizializzato a 0.
  */	
-	public Evento(String titolo, Date data, int numPostiTot) {
+	public Evento(String titolo, LocalDateTime data, int numPostiTot) {
+		
+		/* Inserire il controllo che la data non sia già passata e che il numero di posti totali sia positivo. 
+   In caso contrario mostrare i dovuti avvisi all’utente
+		 */	
+
+		if (LocalDateTime.now().isAfter(data) || numPostiTot <= 0 ) {
+			System.out.println("La data o il numero di posti totali inseriti non sono corretti.");
+		}
+		
 		this.titolo = titolo;
 		this.data = data;
 		this.numPostiTot = numPostiTot;
 		this.numPostiPren = 0;
 	}
 
-/* Inserire il controllo che la data non sia già passata e che il numero di posti totali sia positivo. 
-   In caso contrario mostrare i dovuti avvisi all’utente
- */	
+/* Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
+   -prenota: aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha posti disponibili deve restituire 
+    un messaggio di avviso.
+   -disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono prenotazioni restituisce un 
+    messaggio di avviso.
+	l’override del metodo toString() in modo che venga restituita una stringa contenente: data formattata - titolo
+	Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funzioni richieste.
+ */
+
+	public void prenota() {
+		if (LocalDateTime.now().isAfter(data) || numPostiPren >= numPostiTot) {
+			System.out.println("Non ci sono posti disponibili o l'evento è già passato.");
+		} else {
+			numPostiPren++;
+		}
+	}
 	
+	public void disdici() {
+		if (LocalDateTime.now().isAfter(data) || numPostiPren <= 0) {
+			System.out.println("Non è stato ancora prenotato alcun posto o l'evento è già passato.");
+		} else {
+			numPostiPren--;
+		}
+	}
 	
-	
+	@Override
+	public String toString() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		return data.format(formatter) + " - " + titolo;
+	}
 /* Aggiungere metodi getter e setter in modo che:
 
 	-titolo sia in lettura e in scrittura
@@ -66,12 +85,12 @@ public class Evento {
 	}
 
 
-	public Date getData() {
+	public LocalDateTime getData() {
 		return data;
 	}
 
 
-	public void setData(Date data) {
+	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
 
--- src/org/java/gestore/eventi/Main.java
+++ src/org/java/gestore/eventi/Main.java
@@ -1,10 +1,65 @@
 package org.java.gestore.eventi;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+/* Step 2
+-Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento con tutti i parametri.
+
+-Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni vuole fare e provare ad effettuarle, 
+implementando opportuni controlli
+
+-Stampare a video il numero di posti prenotati e quelli disponibili
+
+-Chiedere all’utente se e quanti posti vuole disdire
+
+-Provare ad effettuare le disdette, implementando opportuni controlli
+
+-Stampare a video il numero di posti prenotati e quelli disponibili
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Inserisci il titolo dell'evento.");
+		String titolo = scan.nextLine();
+		
+		System.out.println("Inserisci la data dell'evento nel formato dd/MM/yyyy HH:mm.");
+		String dataStr = scan.nextLine();
+		// convertire string to localdatetime
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime data = LocalDateTime.parse(dataStr, formatter);
+		
+		System.out.println("Inserisci il numero di posti disponibili per l'evento.");
+		int numPostiTot = scan.nextInt();
+		scan.nextLine();
+		
+		Evento evento = new Evento(titolo, data, numPostiTot);
+		
+		System.out.println("Vuoi prenotare dei posti per l'evento? s/n");
+		String rispostaPrenota = scan.nextLine();
+		
+		if (rispostaPrenota.equalsIgnoreCase("s")) {
+			
+			System.out.println("Quanti posti vuoi prenotare?");
+			int numPrenotazioni = scan.nextInt();
+			
+			for (int i = 0; i < numPrenotazioni; i++) {
+				evento.prenota();
+			}
+			System.out.printf("Hai prenotato %i posti", numPrenotazioni);
+			
+		} 
+		else {
+			System.out.println("Hai scelto di non effettuare prenotazioni.");
+		};
+		
+		
+		
 	}
 
 }
